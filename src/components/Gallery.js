daf390d41b8b62001f3c06ba0f24bb30a64bf5c4
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2020-02-08 13:41:45 +0100

    add gallery section with scrolling function

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,23 @@
 import React, { Component } from "react";
 
+/* require.context() - allows to create your own context. It allows you to pass in a directory to search, a flag indicating whether subdirectories should be searched too, and a regular expression to match files against.
+syntax = require.context(directory, useSubdirectories = true, regExp = /^\.\/.*$/, mode = 'sync'); */
+
+// obj.keys() - returns an array with object's property names
+// arg = context
+// arg.keys() = array of files that fit the context, i.e. arg.keys()[0]='./gal1.jpg'
+// then we use forEach method to create pairs: fileName: fileDirectory
+
+function importAll(arg) {
+  let images = {};
+  arg.keys().forEach(item => {
+    images[item.replace("./", "")] = arg(item);
+  });
+  return images;
+}
+
+const images = importAll(require.context("../img/gallery", false, /\.jpg$/));
+
 class Gallery extends Component {
   state = {};
   render() {
@@ -12,49 +30,109 @@ class Gallery extends Component {
         </p>
         <ul className="photoList">
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal1.jpg"]}
+              alt="monk in the river who is pouring water from bowl"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal2.jpg"]}
+              alt="desert with cloudy sky"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal3.jpg"]}
+              alt="a lady walking among the field"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal4.jpg"]}
+              alt="man dancing with fire"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal5.jpg"]}
+              alt="windmill with cloudy sky in the background"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal6.jpg"]}
+              alt="small wooden boat on the lake with skyscrapers in the background"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal7.jpg"]}
+              alt="fjord during sundown"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal8.jpg"]}
+              alt="a man with surfboard on the beach during sunset"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal9.jpg"]}
+              alt="a tiger in the river"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal10.jpg"]}
+              alt="a soldier with machine gun walking in the fire with parachuters in the background"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal11.jpg"]}
+              alt="two wooden houses among the mountains"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal12.jpg"]}
+              alt="sunset with windmills in the far distance"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal13.jpg"]}
+              alt="a wooden house at the lakeside among the mountains"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal14.jpg"]}
+              alt="foggy forest with wolves on the rock"
+              className="photoList__photo"
+            />
           </li>
           <li className="photoList__item">
-            <img src="" alt="" className="photoList__photo" />
+            <img
+              src={images["gal15.jpg"]}
+              alt="a train on the escarpment in the winter"
+              className="photoList__photo"
+            />
           </li>
         </ul>
       </section>
