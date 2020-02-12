bf2770b4e4cb594c273c72663aed830df633de14
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2020-02-12 11:14:29 +0100

    Updates

---
+++ precache-manifest.861aafc20f11c0247068c827b9f1f59c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58672514c6fc7408088e67f793c69c7",
+    "url": "/static-site-react/index.html"
+  },
+  {
+    "revision": "2a8e2aa6b23c8a3e45e5",
+    "url": "/static-site-react/static/css/main.d68c9061.chunk.css"
+  },
+  {
+    "revision": "407da0b55be88d648dbb",
+    "url": "/static-site-react/static/js/2.25172b3a.chunk.js"
+  },
+  {
+    "revision": "cee262e7168b60b6586afe84e41e797a",
+    "url": "/static-site-react/static/js/2.25172b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8e2aa6b23c8a3e45e5",
+    "url": "/static-site-react/static/js/main.a6473b4f.chunk.js"
+  },
+  {
+    "revision": "215956b1421b872e5b7e",
+    "url": "/static-site-react/static/js/runtime-main.54dc3150.js"
+  },
+  {
+    "revision": "068b211f1160031091419c5f4dade2d3",
+    "url": "/static-site-react/static/media/book.068b211f.jpg"
+  },
+  {
+    "revision": "e66809b14aed615018c699b80eff5fcf",
+    "url": "/static-site-react/static/media/cocktail.e66809b1.svg"
+  },
+  {
+    "revision": "bfd595edaab762dce854a03416a296f5",
+    "url": "/static-site-react/static/media/console.bfd595ed.jpg"
+  },
+  {
+    "revision": "d5b3bca7f00f959f0f6593be520e4ae1",
+    "url": "/static-site-react/static/media/cyberpunk.d5b3bca7.jpg"
+  },
+  {
+    "revision": "08051a1ab6bf0f7c0ee32b3fd769fa12",
+    "url": "/static-site-react/static/media/fogpath.08051a1a.jpg"
+  },
+  {
+    "revision": "e72a990f3fbe91d4a406e792aa93703d",
+    "url": "/static-site-react/static/media/gal1.e72a990f.jpg"
+  },
+  {
+    "revision": "d5c62d29644100c1d1eb7f6119fbd53d",
+    "url": "/static-site-react/static/media/gal10.d5c62d29.jpg"
+  },
+  {
+    "revision": "e68a4f2c452f8c9aa51cb590a79288c2",
+    "url": "/static-site-react/static/media/gal11.e68a4f2c.jpg"
+  },
+  {
+    "revision": "5e8d01bf226a25b9067532de16e6c9e8",
+    "url": "/static-site-react/static/media/gal12.5e8d01bf.jpg"
+  },
+  {
+    "revision": "b35bd937ce7bf5722ff89f5a5fbc8bd3",
+    "url": "/static-site-react/static/media/gal13.b35bd937.jpg"
+  },
+  {
+    "revision": "ae44a8ebc68c16771a2ee7fb5932bae6",
+    "url": "/static-site-react/static/media/gal14.ae44a8eb.jpg"
+  },
+  {
+    "revision": "6038908a65b2bb7d75620e4796326341",
+    "url": "/static-site-react/static/media/gal15.6038908a.jpg"
+  },
+  {
+    "revision": "1f803edcfedd5feac93611666026b490",
+    "url": "/static-site-react/static/media/gal2.1f803edc.jpg"
+  },
+  {
+    "revision": "20b3d1287d5a882350e610362b235987",
+    "url": "/static-site-react/static/media/gal3.20b3d128.jpg"
+  },
+  {
+    "revision": "28ea781d817dd8803fbd1e4136a70e12",
+    "url": "/static-site-react/static/media/gal4.28ea781d.jpg"
+  },
+  {
+    "revision": "78cb036a7eb9a81c28a6cefd59159b73",
+    "url": "/static-site-react/static/media/gal5.78cb036a.jpg"
+  },
+  {
+    "revision": "393f080651c6f94556bd1c001c5563b1",
+    "url": "/static-site-react/static/media/gal6.393f0806.jpg"
+  },
+  {
+    "revision": "4bbb57ee0afeb74884e039ec57174738",
+    "url": "/static-site-react/static/media/gal7.4bbb57ee.jpg"
+  },
+  {
+    "revision": "5da9e3bdb5f3dae88a10ad245172af7f",
+    "url": "/static-site-react/static/media/gal8.5da9e3bd.jpg"
+  },
+  {
+    "revision": "41103ad7bac890518868bbd1a691252e",
+    "url": "/static-site-react/static/media/gal9.41103ad7.jpg"
+  },
+  {
+    "revision": "44c5b5742928ae4fa88940440e040d28",
+    "url": "/static-site-react/static/media/goal.44c5b574.jpg"
+  },
+  {
+    "revision": "44da259213ccf5e5f5c845b965018d56",
+    "url": "/static-site-react/static/media/icons.44da2592.png"
+  },
+  {
+    "revision": "d04dfc48a61c4b198230bfe45ff3e0bc",
+    "url": "/static-site-react/static/media/key.d04dfc48.svg"
+  },
+  {
+    "revision": "aad8607133742e54816023545760d7b7",
+    "url": "/static-site-react/static/media/left.aad86071.svg"
+  },
+  {
+    "revision": "fbb21171188b5afe11265ccd75a5b29d",
+    "url": "/static-site-react/static/media/right.fbb21171.svg"
+  },
+  {
+    "revision": "c51bb65d9010bc3edd2712e9a14940dd",
+    "url": "/static-site-react/static/media/road.c51bb65d.svg"
+  },
+  {
+    "revision": "ca61bc5c8e33990af3ff78242835da9b",
+    "url": "/static-site-react/static/media/stopwatch.ca61bc5c.svg"
+  }
+]);
